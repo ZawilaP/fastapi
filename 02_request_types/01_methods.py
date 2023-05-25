87da1cfe0a116be1768a9742f9f5a96243309950
zawilaniedzwiecki.p <zawilaniedzwiecki.p@pg.com>
2023-05-25 11:49:16 +0200

    methods

--- 02_request_types/01_methods.py
+++ 02_request_types/01_methods.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 
-
 app = FastAPI()
 elements = [1, 2, 3, 4, 5, 6]
 
@@ -28,9 +27,30 @@ async def delete(value: int):  # Add value to list
     if value in elements:
         elements.remove(value)
     return {'message': elements}
+
+
 # Todo!
 assets = {'asset1': 'up', 'asset2': 'up', 'asset3': 'down', 'asset4': 'up', 'asset5': 'down'}
-# Create an API that allows:
-# - listing all assets
-# - listing assets in any state
-# - changing single asset's state
+
+
+@app.get("/assets")
+async def get():
+    return {'message': assets}
+
+
+@app.get('/assets/{state}')
+async def get(state: str):
+    def my_filtering_function(pair):
+        key, value = pair
+        if value == state:
+            return True  # filter pair out of the dictionary
+        else:
+            return False  # keep pair in the filtered dictionary
+
+    return {'message': dict(filter(my_filtering_function, assets.items()))}
+
+
+@app.put('/assets/{state_name}/{state_value}')
+async def get(state_name: str, state_value: str):
+    assets[state_name] = state_value
+    return {'message': f'added element {state_name}: {state_value}'}
